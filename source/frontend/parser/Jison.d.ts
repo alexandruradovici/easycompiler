784a45ec4550b25475915042c8579398f5546cea
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2022-07-11 12:10:08 +0300

    v2

--- source/frontend/parser/Jison.d.ts
+++
@@ -1,8 +0,0 @@
-import { ParseTree } from '@parsetree/nodes';
-import { ParserContext } from '@parsetree/context';
-import { SymbolTable } from '@easycompiler/ast/symbol';
-
-export class Jison
-{
-	parse (text:string, ...fn:Function[]):ParseTree;
-}
